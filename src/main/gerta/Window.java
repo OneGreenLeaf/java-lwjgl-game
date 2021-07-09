9e49b743fd631b9aeec457344e94053ff3c6c6be
Alexander Vashchenko <73034613+OneGreenLeaf@users.noreply.github.com>
2021-07-09 11:27:25 +0300

    Initial commit
    Add key and Mouse Listeners
    
    Add classes for change stages and field for FPS

--- src/main/gerta/Window.java
+++ src/main/gerta/Window.java
@@ -1,9 +1,11 @@
 package main.gerta;
 
+import main.until.Time;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
@@ -12,11 +14,17 @@ public class Window {
     /**
      *
      */
-    private int width, height;
-    private String title;
+    private final int width;
+    private final int height;
+    private final String title;
     private long glfwWindow;
 
+    public float r, g, b, a;
+    private boolean fadeToBlack = false;
+
     private static Window window;
+
+    private static  Scene currentScene;
 
     static {
         window = null;
@@ -26,6 +34,25 @@ public class Window {
         this.width = 800;
         this.height = 600;
         this.title = "Window";
+        r = 1;
+        b = 1;
+        g = 1;
+        a = 1;
+    }
+
+    public static void changeScene(int newScene){
+        switch (newScene){
+            case 0:
+                currentScene = new LevelEditorScene();
+                //currentScene.init();
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                break;
+            default:
+                assert false: "Unknown scene '" + newScene + "'";
+                break;
+        }
     }
 
     public static  Window get(){
@@ -40,66 +67,102 @@ public class Window {
 
         init();
         loop();
+
+        /*
+         Free the memory
+         */
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        /*
+          Terminate GLFW and the free the error callback
+         */
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
     }
 
-    /**
-     * Setup and error callback
+    /*
+      Setup and error callback
      */
     public void init(){
         GLFWErrorCallback.createPrint(System.err).set();
 
-        /**
-         * Initialize GLFW
+        /*
+         Initialize GLFW
          */
         if (!glfwInit()) {
             throw new IllegalStateException("Unable to initialize GLFW.");
         }
-        /**
-         * Configure GLFW
+        /*
+         Configure GLFW
          */
         glfwDefaultWindowHints();
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_FALSE);
 
-        /**
-         * Create the window
+        /*
+          Create the window
          */
         glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
         if(glfwWindow == NULL){
             throw new IllegalStateException("Failed to create the GLFW window.");
         }
-        /**
-         * Make the OpenGL context current
-         * glfwSwapInterval(1) turn On v-sync
+
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
+        /*
+          Make the OpenGL context current
+          glfwSwapInterval(1) turn On v-sync
          */
         glfwMakeContextCurrent(glfwWindow);
         glfwSwapInterval(1);
 
-        /**
-         * Make the window visible
+        /*
+          Make the window visible
          */
         glfwShowWindow(glfwWindow);
 
-        /**
-         * This line is critical for LWJGL's interoperation with GLFW's
-         * OpenGL context, or any context that is managed externally.
-         * LWJGL detects the context that is current in the current thread,
-         * creates the GLCapabilities instance and makes the OpenGL
-         * bindings available for use.
+        /*
+          This line is critical for LWJGL's interoperation with GLFW's
+          OpenGL context, or any context that is managed externally.
+          LWJGL detects the context that is current in the current thread,
+          creates the GLCapabilities instance and makes the OpenGL
+          bindings available for use.
          */
         GL.createCapabilities();
+
+        Window.changeScene(0);
     }
 
     public void loop(){
+        float beginTime = Time.getTime();
+        float endTime;
+        float dt = -1.0f;
+
         while (!glfwWindowShouldClose(glfwWindow)){
             //Poll events
             glfwPollEvents();
 
-            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            if (dt >= 0) {
+                currentScene.update(dt);
+            }
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_ESCAPE)){
+                glfwSetWindowShouldClose(glfwWindow, true);
+            }
+
             glfwSwapBuffers(glfwWindow);
+
+            endTime = Time.getTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
         }
     }
-
 }
