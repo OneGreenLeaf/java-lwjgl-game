c2cdbe53195ccf83f4e37361c0d8ba179a559e6a
Alex Vashchenko <asdw3a@mail.ru>
2021-07-09 14:41:00 +0300

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import main.gerta.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
